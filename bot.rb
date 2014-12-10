9a2136c58bf4d54486b51866fa12ee1fa7c17181
Edmund Dipple <elmundio1987@gmail.com>
2014-12-10 22:25:14 +0000

    plugin-ised the bigbenbong, implemented an early http listener

--- bot.rb
+++ bot.rb
@@ -1,10 +1,41 @@
 require 'cinch'
+require 'socket'
+
+class JenkinsReceiver
+  include Cinch::Plugin
+
+  listen_to :monitor_msg, :method => :send_msg
+
+  def send_msg(m, msg)
+     Channel("#devops").send "Build notification: #{msg}"
+  end
+
+end
+
+class BigBenBong
+  include Cinch::Plugin
+
+  match "big ben",{:use_prefix => false}
+
+  def execute(m)
+    reply = ''
+    time = Time.now.hour
+    if(time > 12) 
+      time = time - 12
+    end
+
+    (1..time).each { |i| reply += "bong " }
+    m.reply reply
+  end
+end
+
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "192.168.59.103"
     c.nick = 'buzz'
     c.channels = ["#devops"]
+    c.plugins.plugins = [JenkinsReceiver,BigBenBong]
   end
 
   on :message, "hello" do |m|
@@ -19,17 +50,20 @@ bot = Cinch::Bot.new do
     m.reply "haha"
   end
 
-  on :message, /big ben/ do |m|
-  	string = ''
-  	time = Time.now.hour
-  	if(time > 12) 
-  		time = time - 12
-  	end
-
-  	(1..time).each { |i| string += "bong " }
-  	m.reply string
-  end  
-
 end
 
+def server(bot)
+  print "Thread Start\n"
+  server = TCPServer.new '127.0.0.1', 2000
+  loop do
+    Thread.start(server.accept) do |client|
+      message = client.gets
+      bot.handlers.dispatch(:monitor_msg, nil, message)
+      client.puts "Ok\n"
+      client.close
+    end #Thread.Start
+  end #loop
+end
+
+Thread.new { server(bot) }
 bot.start
